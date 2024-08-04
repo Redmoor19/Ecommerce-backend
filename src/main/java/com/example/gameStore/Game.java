79ba02020ae664744f2912128e6dd68b51fcfd6e
GreeeenGoo <uladzislau.krukouski@integrify.io>
2024-08-04 09:47:53 +0300

    the very basic functions.

---
+++ src/main/java/com/example/gameStore/Game.java
@@ -0,0 +1,16 @@
+package com.example.gameStore;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Game {
+    private int id;
+    private String name;
+    private String key;
+}
