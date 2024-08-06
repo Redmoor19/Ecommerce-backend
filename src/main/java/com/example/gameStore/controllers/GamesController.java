2124d08e20ddab1e1c7c77c83e7550118b58d514
GreeeenGoo <uladzislau.krukouski@integrify.io>
2024-08-06 17:38:51 +0300

    Added GameService implementation
    GameService implementation was added. Also two more queries: getReviewById and getKeysAmount. Disable wildcard imports.

--- src/main/java/com/example/gameStore/controllers/GamesController.java
+++ src/main/java/com/example/gameStore/controllers/GamesController.java
@@ -4,223 +4,161 @@ package com.example.gameStore.controllers;
 import com.example.gameStore.dtos.GameDto;
 import com.example.gameStore.dtos.KeyDto;
 import com.example.gameStore.dtos.ReviewDto;
-import com.example.gameStore.enums.Genre;
-import com.example.gameStore.enums.PlayerSupport;
+import com.example.gameStore.services.interfaces.GameService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Date;
 import java.util.List;
-import java.util.UUID;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("api/v1/")
 @AllArgsConstructor
 public class GamesController {
 
+    @Autowired
+    private final GameService gameService;
+
     @GetMapping("games")
-    public List<GameDto> findAllGames() {
-        return List.of(
-                new GameDto(UUID.randomUUID(), "Adventure Quest", List.of(Genre.ADVENTURE), 85,
-                        "http://example.com/thumb1.jpg", List.of("http://example.com/image1.jpg"),
-                        "Quest Devs", new Date(), "4GB RAM, 2GB GPU", List.of(PlayerSupport.SINGLE_PLAYER),
-                        19.99f, "An epic adventure game", "SKU12345", true, 4),
-                new GameDto(UUID.randomUUID(), "Space Explorer", List.of(Genre.SIMULATION), 90,
-                        "http://example.com/thumb2.jpg", List.of("http://example.com/image2.jpg"),
-                        "Space Devs", new Date(), "8GB RAM, 4GB GPU", List.of(PlayerSupport.MULTIPLAYER),
-                        29.99f, "Explore the universe", "SKU67890", true, 2),
-                new GameDto(UUID.randomUUID(), "Mystery Mansion", List.of(Genre.PUZZLE), 75,
-                        "http://example.com/thumb3.jpg", List.of("http://example.com/image3.jpg"),
-                        "Mystery Devs", new Date(), "6GB RAM, 3GB GPU", List.of(PlayerSupport.SINGLE_PLAYER),
-                        14.99f, "A spooky horror game", "SKU54321", false, 3)
-        );
+    public ResponseEntity<List<GameDto>> findAllGames() {
+        List<GameDto> games = gameService.findAllGames();
+        if (games.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.ok(games);
     }
 
     @GetMapping("games/{id}")
-    public GameDto getGameById(@PathVariable(required = true, name = "id") UUID id) {
-        return new GameDto(UUID.randomUUID(), "Fantasy World", List.of(Genre.RPG), 88,
-                "http://example.com/thumb4.jpg", List.of("http://example.com/image4.jpg"),
-                "Fantasy Devs", new Date(), "8GB RAM, 4GB GPU", List.of(PlayerSupport.MMO),
-                39.99f, "A magical RPG adventure", "SKU98765", true, 4);
+    public ResponseEntity<GameDto> getGameById(@PathVariable(required = true, name = "id") String id) {
+        Optional<GameDto> game = gameService.getGameById(id);
+        return game.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
 
     @PostMapping("games")
-    public GameDto createGame(@RequestBody GameDto gameDto) {
-        //Creation a new Game;
-        return new GameDto(UUID.randomUUID(), "Cyber City", List.of(Genre.ACTION), 92,
-                "http://example.com/thumb5.jpg", List.of("http://example.com/image5.jpg"),
-                "Cyber Devs", new Date(), "16GB RAM, 6GB GPU", List.of(PlayerSupport.ONLINE_COMPETITIVE),
-                49.99f, "An action-packed cyber adventure", "SKU11223", true, 5);
+    public ResponseEntity<GameDto> createGame(@RequestBody GameDto gameDto) {
+        Optional<GameDto> game = gameService.createGame(gameDto);
+        return game.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
 
     @PatchMapping("games/{id}")
-    public GameDto updateGame(@PathVariable UUID id, @RequestBody GameDto gameDto) {
-        //Updating an existing game
-        return new GameDto(UUID.randomUUID(), "Cyber City", List.of(Genre.ACTION), 92,
-                "http://example.com/thumb5.jpg", List.of("http://example.com/image5.jpg"),
-                "Cyber Devs", new Date(), "16GB RAM, 6GB GPU", List.of(PlayerSupport.ONLINE_COMPETITIVE),
-                49.99f, "An action-packed cyber adventure", "SKU11223", true, 1);
+    public ResponseEntity<GameDto> updateGame(@PathVariable String id, @RequestBody GameDto gameDto) {
+        Optional<GameDto> game = gameService.updateGame(id, gameDto);
+        return game.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
 
     @DeleteMapping("games/{id}")
-    public GameDto deleteGame(@PathVariable UUID id) {
-        //Deleting an existing game
-        return new GameDto(UUID.randomUUID(), "Cyber City", List.of(Genre.ACTION), 92,
-                "http://example.com/thumb5.jpg", List.of("http://example.com/image5.jpg"),
-                "Cyber Devs", new Date(), "16GB RAM, 6GB GPU", List.of(PlayerSupport.ONLINE_COMPETITIVE),
-                49.99f, "An action-packed cyber adventure", "SKU11223", true, 2);
+    public ResponseEntity<Void> deleteGame(@PathVariable String id) {
+        if (gameService.deleteGame(id)) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
     @GetMapping("games/genres")
-    public List<String> getAllGenres() {
-        return Genre.getAllGenres();
+    public ResponseEntity<List<String>> getAllGenres() {
+        List<String> genreList = gameService.getAllGenres();
+        if (genreList.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.ok(genreList);
     }
 
     @GetMapping("games/genres/{genre}")
-    public List<GameDto> getGamesByGenre(@PathVariable String genre) {
-        System.out.println("=====================" + genre + "====================");
-        return List.of(new GameDto(
-                        UUID.randomUUID(), "Epic Saga", List.of(Genre.RPG, Genre.ACTION), 87,
-                        "http://example.com/thumb6.jpg", List.of("http://example.com/image6.jpg"),
-                        "Saga Studios", new Date(), "12GB RAM, 4GB GPU", List.of(PlayerSupport.MULTIPLAYER),
-                        59.99f, "An epic journey through fantastic worlds", "SKU33445", true, 9
-                ),
-
-                new GameDto(
-                        UUID.randomUUID(), "Stealth Ops", List.of(Genre.ACTION, Genre.SHOOTER), 94,
-                        "http://example.com/thumb7.jpg", List.of("http://example.com/image7.jpg"),
-                        "Ops Games", new Date(), "8GB RAM, 3GB GPU", List.of(PlayerSupport.COOPERATIVE),
-                        39.99f, "A thrilling stealth and shooting experience", "SKU55667", true, 8
-                ));
+    public ResponseEntity<List<GameDto>> getGamesByGenre(@PathVariable String genre) {
+        List<GameDto> games = gameService.getGamesByGenre(genre);
+        if (games.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.ok(games);
     }
 
     @GetMapping("users/me/games")
-    public List<GameDto> getCurrentUserGames() {
-        return List.of(new GameDto(
-                        UUID.randomUUID(), "Epic Saga", List.of(Genre.RPG, Genre.ACTION), 87,
-                        "http://example.com/thumb6.jpg", List.of("http://example.com/image6.jpg"),
-                        "Saga Studios", new Date(), "12GB RAM, 4GB GPU", List.of(PlayerSupport.MULTIPLAYER),
-                        59.99f, "An epic journey through fantastic worlds", "SKU33445", true, 3
-                ),
-
-                new GameDto(
-                        UUID.randomUUID(), "Stealth Ops", List.of(Genre.ACTION, Genre.SHOOTER), 94,
-                        "http://example.com/thumb7.jpg", List.of("http://example.com/image7.jpg"),
-                        "Ops Games", new Date(), "8GB RAM, 3GB GPU", List.of(PlayerSupport.COOPERATIVE),
-                        39.99f, "A thrilling stealth and shooting experience", "SKU55667", true, 8
-                ));
+    public ResponseEntity<List<GameDto>> getCurrentUserGames() {
+        List<GameDto> games = gameService.getCurrentUserGames();
+        if (games.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.ok(games);
     }
 
     @GetMapping("users/me/games/favourites")
-    public List<GameDto> getCurrentUserFavouriteGames() {
-        return List.of(new GameDto(
-                        UUID.randomUUID(), "Epic Saga", List.of(Genre.RPG, Genre.ACTION), 87,
-                        "http://example.com/thumb6.jpg", List.of("http://example.com/image6.jpg"),
-                        "Saga Studios", new Date(), "12GB RAM, 4GB GPU", List.of(PlayerSupport.MULTIPLAYER),
-                        59.99f, "An epic journey through fantastic worlds", "SKU33445", true, 4
-                ),
-
-                new GameDto(
-                        UUID.randomUUID(), "Stealth Ops", List.of(Genre.ACTION, Genre.SHOOTER), 94,
-                        "http://example.com/thumb7.jpg", List.of("http://example.com/image7.jpg"),
-                        "Ops Games", new Date(), "8GB RAM, 3GB GPU", List.of(PlayerSupport.COOPERATIVE),
-                        39.99f, "A thrilling stealth and shooting experience", "SKU55667", true, 8
-                ));
+    public ResponseEntity<List<GameDto>> getCurrentUserFavouriteGames() {
+        List<GameDto> games = gameService.getCurrentUserFavouriteGames();
+        if (games.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.ok(games);
     }
 
     @PostMapping("users/me/games/favourites/{gameId}")
-    public GameDto addCurrentUserFavoriteGame(@PathVariable UUID gameId) {
-        //Adding an existing game to favorites;
-        System.out.println("============================" + gameId + "============================");
-        return new GameDto(UUID.randomUUID(), "Cyber City", List.of(Genre.ACTION), 92,
-                "http://example.com/thumb5.jpg", List.of("http://example.com/image5.jpg"),
-                "Cyber Devs", new Date(), "16GB RAM, 6GB GPU", List.of(PlayerSupport.ONLINE_COMPETITIVE),
-                49.99f, "An action-packed cyber adventure", "SKU11223", true, 5);
+    public ResponseEntity<GameDto> addCurrentUserFavoriteGame(@PathVariable String gameId) {
+        Optional<GameDto> game = gameService.addCurrentUserFavoriteGame(gameId);
+        return game.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
 
     @DeleteMapping("users/me/games/favourites/{gameId}")
-    public GameDto deleteCurrentUserFavoriteGame(@PathVariable UUID gameId) {
-        //Deleting a game from favorites;
-        System.out.println("============================" + gameId + "============================");
-        return new GameDto(UUID.randomUUID(), "Cyber City", List.of(Genre.ACTION), 92,
-                "http://example.com/thumb5.jpg", List.of("http://example.com/image5.jpg"),
-                "Cyber Devs", new Date(), "16GB RAM, 6GB GPU", List.of(PlayerSupport.ONLINE_COMPETITIVE),
-                49.99f, "An action-packed cyber adventure", "SKU11223", true, 3);
+    public ResponseEntity<GameDto> deleteCurrentUserFavoriteGame(@PathVariable String gameId) {
+        if (gameService.deleteCurrentUserFavoriteGame(gameId)) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
     @GetMapping("games/{gameId}/reviews")
-    public List<ReviewDto> getGameReviews(@PathVariable UUID gameId) {
-        return List.of(new ReviewDto(
-                UUID.randomUUID(),
-                UUID.randomUUID(),
-                gameId,
-                "Great game, highly recommended!",
-                5,
-                new Date()
-        ), new ReviewDto(
-                UUID.randomUUID(),
-                UUID.randomUUID(),
-                gameId,
-                "It is okay, but a bit boring.",
-                3,
-                new Date()
-        ), new ReviewDto(
-                UUID.randomUUID(),
-                UUID.randomUUID(),
-                gameId,
-                "Not satisfied with the graphic quality. Expected better.",
-                2,
-                new Date()
-        ));
+    public ResponseEntity<List<ReviewDto>> getGameReviews(@PathVariable String gameId) {
+        List<ReviewDto> reviews = gameService.getGameReviews(gameId);
+        if (reviews.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.ok(reviews);
     }
 
-    @PostMapping("games/{gameId}/reviews/{reviewId}")
-    public ReviewDto createReview(@PathVariable UUID gameId, @PathVariable UUID reviewId) {
-        System.out.println("============================" + gameId + "***" + reviewId + "============================");
-        return new ReviewDto(
-                UUID.randomUUID(),
-                UUID.randomUUID(),
-                UUID.randomUUID(),
-                "Fantastic game! Buy it!",
-                5,
-                new Date()
-        );
+    @PostMapping("users/me/games/{gameId}/reviews")
+    public ResponseEntity<ReviewDto> createReview(@PathVariable String gameId) {
+        Optional<ReviewDto> review = gameService.createReview(gameId);
+        return review.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
 
-    @PatchMapping("games/{gameId}/reviews/{reviewId}")
-    public ReviewDto updateReview(@PathVariable UUID gameId, @PathVariable UUID reviewId) {
-        System.out.println("============================" + gameId + "***" + reviewId + "============================");
-        return new ReviewDto(
-                UUID.randomUUID(),
-                UUID.randomUUID(),
-                UUID.randomUUID(),
-                "I didn't like it at all! Don't buy it!",
-                1,
-                new Date()
-        );
+    @PatchMapping("users/me/games/{gameId}/reviews/{reviewId}")
+    public ResponseEntity<ReviewDto> updateReview(@PathVariable String gameId, @PathVariable String reviewId) {
+        Optional<ReviewDto> review = gameService.updateReview(gameId, reviewId);
+        return review.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
 
-    @DeleteMapping("games/{gameId}/reviews/{reviewId}")
-    public ReviewDto deleteReview(@PathVariable UUID gameId, @PathVariable UUID reviewId) {
-        System.out.println("============================" + gameId + "***" + reviewId + "============================");
-        return new ReviewDto(
-                UUID.randomUUID(),
-                UUID.randomUUID(),
-                UUID.randomUUID(),
-                "Hello guy, how is it going? Ohh, wrong app!",
-                1,
-                new Date()
-        );
+    @DeleteMapping("users/me/games/{gameId}/reviews/{reviewId}")
+    public ResponseEntity<ReviewDto> deleteReview(@PathVariable String gameId, @PathVariable String reviewId) {
+        if (gameService.deleteReview(gameId, reviewId)) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @GetMapping("games/{gameId}/reviews/{reviewId}")
+    public ResponseEntity<ReviewDto> getReviewById(@PathVariable String gameId, @PathVariable String reviewId) {
+        Optional<ReviewDto> game = gameService.getReviewById(gameId, reviewId);
+        return game.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
 
     @PostMapping("games/{gameId}/keys")
-    public KeyDto addKeyToGame(@PathVariable UUID gameId) {
-        System.out.println("============================" + gameId + "============================");
-        return new KeyDto(
-                UUID.randomUUID(),
-                UUID.randomUUID(),
-                new Date(),
-                gameId
-        );
+    public ResponseEntity<KeyDto> addKeyToGame(@PathVariable String gameId) {
+        Optional<KeyDto> key = gameService.addKeyToGame(gameId);
+        return key.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
+
+    @GetMapping("games/{gameId}/keys")
+    public ResponseEntity<Integer> getGameKeysAmount(@PathVariable String gameId) {
+        Optional<Integer> keysAmount = gameService.countGameKeys(gameId);
+        return keysAmount.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
 }
--- src/main/java/com/example/gameStore/services/interfaces/GameService.java
+++ src/main/java/com/example/gameStore/services/interfaces/GameService.java
@@ -1,7 +1,47 @@
 package com.example.gameStore.services.interfaces;
 
 import com.example.gameStore.dtos.GameDto;
+import com.example.gameStore.dtos.KeyDto;
+import com.example.gameStore.dtos.ReviewDto;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface GameService {
-    public GameDto getAcademicPlan(String id);
+
+    public List<GameDto> findAllGames();
+
+    public Optional<GameDto> getGameById(String id);
+
+    public Optional<GameDto> createGame(GameDto gameDto);
+
+    public Optional<GameDto> updateGame(String id, GameDto gameDto);
+
+    public boolean deleteGame(String id);
+
+    public List<String> getAllGenres();
+
+    public List<GameDto> getGamesByGenre(String genre);
+
+    public List<GameDto> getCurrentUserGames();
+
+    public List<GameDto> getCurrentUserFavouriteGames();
+
+    public Optional<GameDto> addCurrentUserFavoriteGame(String gameId);
+
+    public boolean deleteCurrentUserFavoriteGame(String gameId);
+
+    public List<ReviewDto> getGameReviews(String gameId);
+
+    public Optional<ReviewDto> createReview(String gameId);
+
+    public Optional<ReviewDto> updateReview(String gameId, String reviewId);
+
+    public boolean deleteReview(String gameId, String reviewId);
+
+    public Optional<ReviewDto> getReviewById(String gameId, String reviewId);
+
+    public Optional<KeyDto> addKeyToGame(String gameId);
+
+    public Optional<Integer> countGameKeys(String gameId);
 }
