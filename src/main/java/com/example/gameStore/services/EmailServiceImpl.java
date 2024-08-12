3842fb2cbfbc573e9b67b273102e1b488912f7b6
Olshanskaya <viktoriarubenovna@gmail.com>
2024-08-12 09:28:38 +0300

    send correct email with game-keys

--- src/main/java/com/example/gameStore/services/EmailServiceImpl.java
+++ src/main/java/com/example/gameStore/services/EmailServiceImpl.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @Service
 public class EmailServiceImpl implements EmailService {
@@ -15,15 +16,17 @@ public class EmailServiceImpl implements EmailService {
     @Autowired
     private JavaMailSender emailSender;
 
-    private final String userText = """
-            There are your keys for the game you have purchased:""";
+    private final String getKeysText = """
+            There are your keys for the game you have purchased: \n""";
 
     public void sendMessagePurchasedKeys(String email, Map<String, List<String>> keys) {
         SimpleMailMessage message = new SimpleMailMessage();
         message.setFrom("fake.game.store.app@gmail.com");
         message.setTo(email);
         message.setSubject("Game keys");
-        message.setText(userText);
+        message.setText(getKeysText + keys.entrySet().stream()
+                .map(entry -> entry.getKey() + ": " + entry.getValue())
+                .collect(Collectors.joining("\n")));
         emailSender.send(message);
     }
 }
