9a9ed9eec0298af2164cc1245b5ef14863b4773b
Olshanskaya <viktoriarubenovna@gmail.com>
2024-08-12 13:00:22 +0300

    new sendMessage function

--- src/main/java/com/example/gameStore/services/EmailServiceImpl.java
+++ src/main/java/com/example/gameStore/services/EmailServiceImpl.java
@@ -16,17 +16,24 @@ public class EmailServiceImpl implements EmailService {
     @Autowired
     private JavaMailSender emailSender;
 
-    private final String getKeysText = """
+    private final String GET_KEYS_TEXT = """
             There are your keys for the game you have purchased: \n""";
+    private final String GET_KEYS_SUBJECT = "Game keys";
 
     public void sendMessagePurchasedKeys(String email, Map<String, List<String>> keys) {
+        String textKeys = keys.entrySet().stream()
+                .map(entry -> entry.getKey() + ": " + entry.getValue())
+                .collect(Collectors.joining("\n"));
+
+        sendMessage(email, GET_KEYS_SUBJECT, GET_KEYS_TEXT + textKeys);
+    }
+
+    private void sendMessage(String email, String subject, String text) {
         SimpleMailMessage message = new SimpleMailMessage();
         message.setFrom("fake.game.store.app@gmail.com");
         message.setTo(email);
-        message.setSubject("Game keys");
-        message.setText(getKeysText + keys.entrySet().stream()
-                .map(entry -> entry.getKey() + ": " + entry.getValue())
-                .collect(Collectors.joining("\n")));
+        message.setSubject(subject);
+        message.setText(text);
         emailSender.send(message);
     }
 }
