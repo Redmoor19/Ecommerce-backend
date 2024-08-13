a90f6089e51975b11c677feb0c21fae8216b1aa7
Olshanskaya <viktoriarubenovna@gmail.com>
2024-08-13 21:40:12 +0300

    ResourceNotFoundException

--- src/main/java/com/example/gameStore/shared/GlobalExceptionHandler.java
+++ src/main/java/com/example/gameStore/shared/GlobalExceptionHandler.java
@@ -1,14 +1,21 @@
 package com.example.gameStore.shared;
 
 import com.example.gameStore.shared.exceptions.BadRequestException;
+import com.example.gameStore.shared.exceptions.ResourceNotFoundException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
+
     @ExceptionHandler(BadRequestException.class)
     public ResponseEntity<String> handleBadRequestException(RuntimeException e) {
         return ResponseEntity.badRequest().body(e.getMessage());
+    }
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<String> handleResourceNotFoundException(RuntimeException e) {
+        return ResponseEntity.notFound().build();
     }
 }
