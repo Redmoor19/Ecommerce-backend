dda36af0271cc0887a41fe0151feb650bf909a84
Olshanskaya <viktoriarubenovna@gmail.com>
2024-08-14 13:20:43 +0300

    GlobalResponse added Error

--- src/main/java/com/example/gameStore/dtos/GlobalResponse.java
+++ src/main/java/com/example/gameStore/dtos/GlobalResponse.java
@@ -1,14 +1,35 @@
 package com.example.gameStore.dtos;
 
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
 public class GlobalResponse<T> {
-    private final T data;
-    private final String message;
+    public final static String SUCCESS = "success";
+    public final static String ERROR = "error";
 
-    public GlobalResponse(T data, String message) {
+    private final T data;
+    private final String status;
+    private final Error error;
+
+    public GlobalResponse(T data, String status, Integer errorCode, String errorMessage) {
         this.data = data;
-        this.message = message;
+        this.status = status;
+        if(errorCode == null || errorMessage == null) {
+            this.error = null;
+            return;
+        }
+        this.error = new Error(errorMessage, errorCode);
+    }
+
+    @Getter
+    public static class Error {
+        private final String errorMessage;
+        private final Integer errorCode;
+
+        public Error(String message, Integer code) {
+            this.errorMessage = message;
+            this.errorCode = code;
+        }
     }
 }
--- src/main/java/com/example/gameStore/shared/GlobalExceptionHandler.java
+++ src/main/java/com/example/gameStore/shared/GlobalExceptionHandler.java
@@ -9,6 +9,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
 import static org.springframework.http.HttpStatus.NOT_FOUND;
 import static org.springframework.http.HttpStatus.NO_CONTENT;
 
@@ -17,33 +19,33 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(BadRequestException.class)
     public ResponseEntity<GlobalResponse<Void>> handleBadRequestException(RuntimeException e) {
-        return ResponseEntity.badRequest().body(new GlobalResponse<>(null, e.getMessage()));
+        return ResponseEntity.badRequest().body(new GlobalResponse<>(null, GlobalResponse.ERROR, BAD_REQUEST.value(), e.getMessage()));
     }
 
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<GlobalResponse<Void>> handleResourceNotFoundException(RuntimeException e) {
         return ResponseEntity
                 .status(NOT_FOUND)
-                .body(new GlobalResponse<>(null, e.getMessage()));
+                .body(new GlobalResponse<>(null, GlobalResponse.ERROR, NOT_FOUND.value(), e.getMessage()));
     }
 
     @ExceptionHandler(ResourceAlreadyExistsException.class)
     public ResponseEntity<GlobalResponse<Void>> handleResourceAlreadyExistsException(RuntimeException e) {
-        return ResponseEntity.badRequest().body(new GlobalResponse<>(null, e.getMessage()));
+        return ResponseEntity.badRequest().body(new GlobalResponse<>(null, GlobalResponse.ERROR, BAD_REQUEST.value(), e.getMessage()));
     }
 
     @ExceptionHandler(NoContentException.class)
     public ResponseEntity<GlobalResponse<Void>> handleNoContentException(RuntimeException e) {
-        return ResponseEntity.status(NO_CONTENT).body(new GlobalResponse<>(null, e.getMessage()));
+        return ResponseEntity.status(NO_CONTENT).body(new GlobalResponse<>(null, GlobalResponse.ERROR, NO_CONTENT.value(), e.getMessage()));
     }
 
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<GlobalResponse<Void>> handleIllegalArgumentException(RuntimeException e) {
-        return ResponseEntity.badRequest().body(new GlobalResponse<>(null, e.getMessage()));
+        return ResponseEntity.badRequest().body(new GlobalResponse<>(null, GlobalResponse.ERROR, BAD_REQUEST.value(), e.getMessage()));
     }
 
     @ExceptionHandler(RuntimeException.class)
     public ResponseEntity<GlobalResponse<Void>> handleRuntimeException(RuntimeException e) {
-        return ResponseEntity.internalServerError().body(new GlobalResponse<>(null, e.getMessage()));
+        return ResponseEntity.internalServerError().body(new GlobalResponse<>(null, GlobalResponse.ERROR, INTERNAL_SERVER_ERROR.value(), e.getMessage()));
     }
 }
